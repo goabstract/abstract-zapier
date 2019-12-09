d8650705c0477371c87a68b1315c5ca2afe58ba0
bitpshr <mail@bitpshr.net>
2019-12-09 11:59:20 -0500

    feat: add additional events

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { version: platformVersion } = require("zapier-platform-core");
 const authentication = require("./lib/authentication");
 const branchCreated = require("./lib/triggers/branchCreated");
+const branchStatusUpdated = require("./lib/triggers/branchStatusUpdated");
+const branchUpdated = require("./lib/triggers/branchUpdated");
+const commentCreated = require("./lib/triggers/commentCreated");
+const commentDeleted = require("./lib/triggers/commentDeleted");
+const commentUpdated = require("./lib/triggers/commentUpdated");
+const commitsCreated = require("./lib/triggers/commitsCreated");
 const organizationsExist = require("./lib/triggers/organizationsExist");
+const projectCreated = require("./lib/triggers/projectCreated");
+const projectDeleted = require("./lib/triggers/projectDeleted");
+const projectUpdated = require("./lib/triggers/projectUpdated");
 const { version } = require("./package.json");
 
 const AbstractZap = {
@@ -13,7 +22,16 @@ const AbstractZap = {
 
   triggers: {
     [branchCreated.key]: branchCreated,
-    [organizationsExist.key]: organizationsExist
+    [branchStatusUpdated.key]: branchStatusUpdated,
+    [branchUpdated.key]: branchUpdated,
+    [commentCreated.key]: commentCreated,
+    [commentDeleted.key]: commentDeleted,
+    [commentUpdated.key]: commentUpdated,
+    [commitsCreated.key]: commitsCreated,
+    [organizationsExist.key]: organizationsExist,
+    [projectCreated.key]: projectCreated,
+    [projectDeleted.key]: projectDeleted,
+    [projectUpdated.key]: projectUpdated
   }
 };
 
--- test/index.test.js
+++ test/index.test.js
@@ -1,26 +1,46 @@
 const zapier = require("zapier-platform-core");
 const AbstractZap = require("../index");
 const branchCreated = require("../lib/triggers/branchCreated");
+const branchStatusUpdated = require("../lib/triggers/branchStatusUpdated");
+const branchUpdated = require("../lib/triggers/branchUpdated");
+const commentCreated = require("../lib/triggers/commentCreated");
+const commentDeleted = require("../lib/triggers/commentDeleted");
+const commentUpdated = require("../lib/triggers/commentUpdated");
+const commitsCreated = require("../lib/triggers/commitsCreated");
+const projectCreated = require("../lib/triggers/projectCreated");
+const projectDeleted = require("../lib/triggers/projectDeleted");
+const projectUpdated = require("../lib/triggers/projectUpdated");
 
 const appTester = zapier.createAppTester(AbstractZap);
 
-describe("AbstractZap", () => {
-  test("branch.created", async () => {
+const testTrigger = trigger => {
+  return test(trigger.key, async () => {
     const bundle = {
       cleanedRequest: {
         data: {
-          object: {
-            branch: branchCreated.operation.sample
-          }
+          object: trigger.operation.sample
         }
       }
     };
 
-    const results = await appTester(branchCreated.operation.perform, bundle);
+    const results = await appTester(trigger.operation.perform, bundle);
     const returnedEntity = results[0];
     const cachedEntity = bundle.cleanedRequest.data.object;
 
     expect(results.length).toBe(1);
-    expect(returnedEntity.sha).toEqual(cachedEntity.sha);
+    expect(returnedEntity).toEqual(cachedEntity);
   });
+};
+
+describe("AbstractZap", () => {
+  testTrigger(branchCreated);
+  testTrigger(branchStatusUpdated);
+  testTrigger(branchUpdated);
+  testTrigger(commentCreated);
+  testTrigger(commentDeleted);
+  testTrigger(commentUpdated);
+  testTrigger(commitsCreated);
+  testTrigger(projectCreated);
+  testTrigger(projectDeleted);
+  testTrigger(projectUpdated);
 });
